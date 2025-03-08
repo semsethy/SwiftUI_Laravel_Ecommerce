9054999fc1710f2f17c28737e788bedc27b73fa0
semsethy <sethyrisk@gmail.com>
2025-03-08 18:52:27 +0700

    Initial Commit

---
+++ SwiftUI_Laravel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_Laravel
+//
+//  Created by JoshipTy on 8/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_Laravel/SwiftUI_LaravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_LaravelApp.swift
+//  SwiftUI_Laravel
+//
+//  Created by JoshipTy on 8/3/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_LaravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
